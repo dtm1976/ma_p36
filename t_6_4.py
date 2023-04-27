ea722c4d8a2d8657a043f297a146027af58d81cf
Taras Dalyak <tdalyak@gmail.com>
2023-04-27 21:22:21 +0300

    added doc-string to t_6_4.py

--- t_6_4.py
+++ t_6_4.py
@@ -1,4 +1,11 @@
 def find_duration(x, y, percent=10):
+    """
+    percentage calculation -
+    :param x: int - start number
+    :param y: int - target number
+    :param percent: int - percentage value
+    :return: iteration number
+    """
     p = 0.01 * percent
     res = x
     it = 1
